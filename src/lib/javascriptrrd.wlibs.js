57ee6a0bddc8f8137e7c859f3936ab6e4fd539e5
erwinlem <erwin.github@bk3.nl>
2015-08-13 23:19:57 +0200

    replaced makefile with dynamic js include

---
+++ src/lib/javascriptrrd.wlibs.js
@@ -0,0 +1,36 @@
+// get directory of js file
+var scripts = document.getElementsByTagName("script"),
+src = scripts[scripts.length-1].src;
+base = (src.substr(0, src.lastIndexOf('/')+1));
+
+// javascript has no proper include function, i mean like, wtf?
+// http://stackoverflow.com/questions/950087/include-a-javascript-file-in-another-javascript-file
+function includeJS(s) {
+	for(i=0;i<s.length;i++){
+		var script=document.createElement("script");
+		script.type="text/javascript";
+		script.src=base+s[i];
+		document.getElementsByTagName("head")[0].appendChild(script)
+	};
+}
+
+// include rrd
+includeJS([
+		"binaryXHR.js",
+		"rrdFile.js",
+		"rrdFlotSupport.js",
+		"rrdFlot.js",
+		"rrdFlotMatrix.js",
+		"rrdFilter.js",
+		"rrdMultiFile.js",
+		"rrdFlotAsync.js"
+]); 
+
+
+// include flot
+includeJS([
+		"../../external/flot/jquery.js", 
+		"../../external/flot/jquery.flot.js",
+		"../../external/flot/jquery.flot.time.js",
+		"../../external/flot/jquery.flot.selection.js"
+]);

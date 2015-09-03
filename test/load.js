b628d6d2184f5c8c78f942fcfe3a7cfc88f5cfdd
erwinlem <erwin.github@bk3.nl>
2015-09-03 13:20:45 +0200

    fixing file loading under nodesj + coverall

--- test/load.js
+++ test/load.js
@@ -1,10 +1,17 @@
 var expect = require("chai").expect;
-var binaryXHR = require("../lib/binaryXHR.js").FetchBinaryURLAsync;
+var binaryXHR = require("../lib/binaryXHR.js").FetchBinaryURL;
+var RRDFile = require("../lib/rrdFile.js").RRDFile;
+var assert = require('assert');
 
-describe("rrd", function(){
-	describe("constructor", function() {
-		binaryXHR("example_rrds/example3.rrd", function (bf) {
-			var i_rrd_data=new tags.RRDFile(bf);
-		});	    
+describe("file loading", function(){
+
+	it("load file", function() {
+		var bf = binaryXHR("example_rrds/example3.rrd");
+		assert.equal(bf.getCStringAt(0,3),"RRD");
+	});	    
+
+	it("load rrd file", function() {
+		var bf = binaryXHR("example_rrds/example3.rrd");
+		var rrd_data = new RRDFile(bf);
 	});	    
 });
